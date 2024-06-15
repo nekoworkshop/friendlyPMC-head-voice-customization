04621634709a85da3e93f53137576ac912dcc736
Alexandru Ghioroae <alexandru.ghioroae@gmail.com>
2024-06-16 01:45:12 +0300

    #in-progress, "Contact" command

--- client/Actions/FollowerEnemyScan.cs
+++ client/Actions/FollowerEnemyScan.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 namespace friendlyPMC.Actions
 {
@@ -13,25 +14,82 @@ namespace friendlyPMC.Actions
     {
         public static void ScanDirection(BotOwner bot, IPlayer player)
         {
-            Vector3 direction = player.LookDirection;
-            
-            Components.Logger.LogInfo("Scan for Enemy");
+            Components.Logger.LogInfo("Scan for Enemies");
 
-            RaycastHit[] hits = Physics.RaycastAll(player.Transform.position, direction, 150f, LayerMaskClass.PlayerMask);
+            List<Player> enemies = new List<Player>();
 
-            foreach (RaycastHit hit in hits)
+            Vector3 playerPosition = player.Transform.position;
+            Vector3 playerLookDirection = player.LookDirection;
+            float sphereRadius = friendlyPMC.scanDistance.Value / 2;
+            float sphereDistance = friendlyPMC.scanDistance.Value / 2;
+
+            RaycastHit[] hits = new RaycastHit[10];
+
+            int numHits = Physics.SphereCastNonAlloc(
+                    new Ray(playerPosition, playerLookDirection),
+                    sphereRadius,
+                    hits,
+                    sphereDistance,
+                     LayerMaskClass.PlayerMask
+                );
+
+            // get all enemies the boss might have seen
+            for (int i = 0; i < numHits; i++)
             {
-                // Check if the hit object is an enemy
+                RaycastHit hit = hits[i];
                 if (hit.collider != null)
                 {
                     var enemy = bot.ShootData.method_4(hit.collider);
-                    if (enemy != null && enemy.HealthController.IsAlive && enemy.Side != player.Side)
+                    
+                    if (enemy != null && enemy.IsAI && enemy.HealthController.IsAlive && enemy.Side != player.Side)
                     {
-                        Components.Logger.LogInfo("Enemy Found");
-                        bot.BotsGroup.AddEnemy(enemy, EBotEnemyCause.initCauseEnemy);
-                        break;
+                        Components.Logger.LogInfo("Add enemy to the list");
+                        enemies.Add(enemy);
                     }
                 }
+            }
+            // make the close one the priority
+            float dist = Mathf.Infinity;
+            Player closet = null;
+            foreach (var item in enemies)
+            {
+                float edist = Vector3.Distance(player.Transform.position, item.Position);
+                if (edist < dist)
+                {
+                    dist = edist;
+                    closet = item;
+                }
+            }
+
+            if ( closet != null )
+            {
+                bot.BotsGroup.AddEnemy(closet.AIData.Player, EBotEnemyCause.addBotAtGroup);
+                
+                if (bot.Memory.HaveEnemy) return;
+
+                Components.Logger.LogInfo("Add closest enemy to the group");
+                Vector3 direction = closet.Position - playerPosition;
+                float distance = direction.magnitude;
+                RaycastHit hit;
+
+                if(Physics.Raycast(player.PlayerBones.Head.position, direction,out hit, distance, LayerMaskClass.HighPolyWithTerrainMask)) {
+                    if(hit.collider != null)
+                    {
+                        var aiHit = bot.ShootData.method_4(hit.collider);
+                        if (aiHit != null && aiHit.ProfileId == closet.ProfileId)
+                        {
+                            EnemyInfo info;
+                            bot.EnemiesController.EnemyInfos.TryGetValue(aiHit.AIData.Player, out info);
+                            if(info != null)
+                            {
+                                Components.Logger.LogInfo("Make closest enemy a priority");
+                                //info.SetVisible(true);
+                                bot.Memory.GoalEnemy = info;
+                            }
+                        }
+                    }
+                }
+                
             }
         }
     }
